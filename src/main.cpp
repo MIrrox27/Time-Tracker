e3010eeafb75b8631f92661e6527f59dcecea767
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-07-12 12:09:56 +0500

    добавлен основной файл, с  подключенными библиотеками

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+// author github.com/MIrrox27/Time-Tracker
+// src/main.cpp
+
+#include <windows.h>
+#include <iostream>
+#include <map>
+#include <string> 
+#include <fstream>
+#include <chrono>
+
+
+int main(){
+
+
+  return 0;
+}
